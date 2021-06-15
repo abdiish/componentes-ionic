87ffc841e0daf70771795709bd0e928850af5666
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-15 01:10:50 -0500

    Se implementa el uso de ion-item-sliding, se imprimen datos en consola

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonList } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { DataService } from '../../services/data.service';
 
@@ -12,15 +13,36 @@ export class ListPage implements OnInit {
   //Se crea un objeto, Observable de tipo any
   usuarios: Observable<any>;
 
+  //Hacer referencia a componente html ion-list
+  @ViewChild(IonList) ionList: IonList;
+
+
   //Llamada al servicio, se inyecta
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
 
     this.usuarios = this.dataService.getUsuario();
-    
+
   }
 
+  favorite(user: any) {
 
+    console.log('favorite', user);
+    this.ionList.closeSlidingItems();
+
+  }
+
+  share(user: any) {
+
+    console.log('share', user);
+    this.ionList.closeSlidingItems();
+  }
+
+  unread(user: any) {
+
+    console.log('delete', user.name);
+    this.ionList.closeSlidingItems();
+  }
 
 }
