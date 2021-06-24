d3f78675735f5777eb68af11cb63ccb1c3081188
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-23 23:17:01 -0500

    Se implementa ion-menu, se crea el archivo interfaces.ts y se crea archivo menu-opts.json para las opciones del menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componente } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+
+  componentes: Observable<Componente[]>;
+
+  constructor(private menuCtrl: MenuController, 
+              private dataService: DataService) { }
+
+  ngOnInit() {
+      this.componentes = this.dataService.getMenuOpts();
+  }
+
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Componente } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,14 @@ export class DataService {
   //Se crea una función 
   getUsuario() {
     
-    return this.http.get('https://jsonplaceholder.typicode.com/users')
-    
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+
   }
+
+  getMenuOpts() {
+    
+    return this.http.get<Componente[]>('/assets/data/menu-opts.json');
+
+  }
+
 }
